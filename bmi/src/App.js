4f93bcf44a0090329187132969a4bb4dfac3ae61
Ara Developer <loma1016@gmail.com>
2020-06-30 01:52:43 +0900

    [기능 추가] : Calculate bmi
    height, weight 값을 이용하여 bmi 계산 추가.
    
    [추가 필요] 
    - bmi 계산 값은 소수 2번째 자리까지 출력
    - ui style 수정
    - input 값 뒤에 cm, kg 단위 출력

--- bmi/src/App.js
+++ bmi/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './Components/Input/Input';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Input />
+    </div>
+  );
 }
 
 export default App;
--- bmi/src/Components/Input/Input.js
+++ bmi/src/Components/Input/Input.js
@@ -11,17 +11,29 @@ export default class Input extends React.Component {
   }
 
   handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    const { height, weight } = this.state;
+    if (e.target.name === 'height') {
+      this.setState({ height: e.target.value });
+    } else if (e.target.name === 'weight') {
+      this.setState({ weight: e.target.value });
+    }
+    this.calculateBmi(height, weight);
+  };
+
+  calculateBmi = (height, weight) => {
+    // console.log(`height : ${height} ||`)
+    let bmiHeight = height / 100;
+    let bmi = weight / (bmiHeight * bmiHeight);
+    console.log(bmi);
   };
 
   render() {
     const { handleChange } = this;
+    // const { height, weight } = this.state;
     return (
       <div className='input-wrap'>
-        <input className='input-box' type='text' name='height' onChange={handleChange}></input>
-        <input className='input-box' type='text' name='weight' onChange={handleChange}></input>
+        <input className='input-box' type='number' name='height' onChange={handleChange} placeholder='키 : ex) 175cm'></input>
+        <input className='input-box' type='number' name='weight' onChange={handleChange} placeholder='몸무게 : ex) 65kg'></input>
       </div>
     );
   }
