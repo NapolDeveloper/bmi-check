c02b20b5638ec85c2b987351e59f6bb31481d8bc
Ara Developer <loma1016@gmail.com>
2020-07-03 00:54:21 +0900

    [메인 수정] : merge후 코드 수정

--- bmi/src/App.js
+++ bmi/src/App.js
@@ -5,13 +5,8 @@ import Input from './Components/Input/Input';
 import Container from './Components/Container/Container';
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <Input />
-=======
     <div className='body-wrap'>
       <Container />
->>>>>>> container
     </div>
   );
 }
